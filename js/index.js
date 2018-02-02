19c5ee4871c191401e941b83c0d94ef67506eeb8
Reid South <rbsouth@gmail.com>
2018-02-02 13:44:47 -0700

    feel my frustration

---
+++ js/index.js
@@ -0,0 +1,54 @@
+
+onezero = '10'
+
+function stringy(size) {
+	for (var i = 0; i < size.length; i++) {
+		console.log(onezero)
+		console.log(size[i])
+	}
+	console.log(onezero) * size
+		//print(size.length[i] = '10')
+		//console.log(size[i]);
+	
+}
+var number = ['', '', '', '', '']
+console.log(number);
+stringy(number);
+var int = 214620
+
+function reverse(number){
+	number = number + '';
+	console.log(number);
+	numarr = [''];
+	for (var i = number.length; i >= 0 ; i--) {
+		number[i]
+		console.log(number[i])
+		numarr = [number[i]]
+		console.log(numarr)
+	}
+
+	// function reverseIntAsArray(int) {
+	// 	let numsAsString = int.toString();
+	// 	let newArr = [];
+	// 	for (var i = numsAsString.length - 1; i >= 0; i--) {
+	// 		newArr.push(parseInt(numsAsString[i]));
+	// 		}
+	// 		return newArr;
+	// 	}
+
+	// 	console.log(reverseIntAsArray(378407));
+
+		
+	for (var i = 0; i < number.length; i++) {
+		number[i];
+		console.log(number[i]);
+		numarr = [number[i]];
+		console.log(numarr);
+	}
+	numarr = [number]
+	console.log(numarr)
+
+	}
+}
+
+reverse(int);
